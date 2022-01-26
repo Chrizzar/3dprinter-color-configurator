8b6cfa30b377d1f45ebe1ade95eccc72a52ca59f
Christian Sass Hansen <36627355+Chrizzar@users.noreply.github.com>
2022-01-26 15:11:50 +0100

    Added new menu to main site

---
+++ scripts/main-footer.js
@@ -0,0 +1,20 @@
+document.write('\
+\
+<div class="footer">\
+    <div class="footer-content">\
+        <h3>About</h3>\
+        <p>Our mission with this tool is to make it more convenient for the user to visualize what they want their 3D printer color theme to look like. Users can now create and visualize very unique looking builds, where currently up to 8 different groups of parts can be colored (this holds for Voron printers).</p>\
+        <ul class="socials">\
+            <!-- Discord Maybe -->\
+            <!--<li><a href="#"><i class="fa fa-facebook"></i></a></li>-->\
+            <li><a href="https://twitter.com/Chrizzar" target="_blank" aria-label="Link to my Twitter"><i class="fa fa-twitter twitter"></i></a></li>\
+            <li><a href="https://www.youtube.com/channel/UC7DucVUUyAl8whkK8KzqBhA" target="_blank" aria-label="Link to my YouTube"><i class="fa fa-youtube youtube"></i></a></li>\
+            <li><a href="https://www.linkedin.com/in/christian-sass-hansen-639571188/" target="_blank" aria-label="Link to my Linkedin"><i class="fa fa-linkedin-square linkedin"></i></a></li>\
+        </ul>\
+    </div>\
+    <div class="footer-bottom">\
+        <p>copyright &copy; 2021-2022 <span>Christian Sass Hansen</span> - All Rights Reserved.</p>\
+    </div>\
+</div>\
+\
+');
---
+++ scripts/main-navbar.js
@@ -0,0 +1,30 @@
+document.write('\
+\
+<header>\
+    <nav class="new-navbar">\
+        <div class="branding"><a href="index.html" class="home">Home</a>\
+        </div>\
+\
+        <div class="hamburger-spacer">\
+            <label for="input-hamburger" class="hamburger"></label>\
+        </div>\
+        <input type="checkbox" id="input-hamburger" hidden>\
+\
+        <ul class="menu">\
+            <li class="has-dropdown">\
+                <a style="cursor: pointer;" class="menu-link">About us\
+                    <span class="arrow"></span>\
+                </a>\
+                <ul class="submenu">\
+                    <li><a href="contact.html" class="menu-link">Contact</a></li>\
+                    <li><a href="terms-of-use.html" class="menu-link">Terms of use</a></li>\
+                    <li><a href="feedback.html" class="menu-link">Feedback</a></li>\
+                </ul>\
+            </li>\
+            <li><a href="roadmap.html">Roadmap</a></li>\
+            <li><a href="support-me.html" style="font-weight: bold;">Support Me</a></li>\
+        </ul>\
+    </nav>\
+</header>\
+\
+');
