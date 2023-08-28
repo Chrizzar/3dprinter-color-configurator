01402e9a940dfb604e4308ae2645dc9d2a502103
Christian Sass Hansen <fotochrissar@gmail.com>
2023-08-28 10:29:51 +0200

    Fixed navbar at front page

--- scripts/main-navbar.js
+++ scripts/main-navbar.js
@@ -1,8 +1,7 @@
 document.write('\
 \
-<!-- Navbar -->\
 <header>\
-    <a href="../voron-color-configurator.html" class="logo">Home</a>\
+    <a href="index.html" class="logo">Home</a>\
 \
     <input type="checkbox" id="menu-bar">\
     <label for="menu-bar" class="icon">\
@@ -15,55 +14,15 @@ document.write('\
 \
     <nav class="new-navbar">\
         <ul>\
-            <!-- Voron V0.1 -->\
-            <li><a href="#">Voron V0.2R1 <i class="fa fa-caret-down"></i></a>\
-                <ul>\
-                    <li><a href="voron-color-configurator/voron-v0.2-default.html">Default</a></li>\
-                    <li><a href="voron-color-configurator/voron-v0.2-8accents.html">8 Accents</a></li>\
-                </ul>\
-            </li>\
-            <!-- Voron Trident -->\
-            <li><a href="#">Voron Trident <i class="fa fa-caret-down"></i></a>\
-                <ul>\
-                    <li><a href="voron-color-configurator/voron-trident-default.html">Default</a></li>\
-                    <li><a href="voron-color-configurator/voron-trident-8accents.html">8 Accents</a></li>\
-                </ul>\
-            </li>\
-            <!-- Voron V2.4 -->\
-            <li><a href="#">Voron V2.4 <i class="fa fa-caret-down"></i></a>\
-                <ul>\
-                    <li><a href="voron-color-configurator/voron-v2.4-default.html">Default</a></li>\
-                    <li><a href="voron-color-configurator/voron-v2.4-7accents.html">7 Accents</a></li>\
-                    <li><a href="voron-color-configurator/voron-v2.4-8accents.html">8 Accents</a></li>\
-                </ul>\
-            </li>\
-            <!-- Voron Switchwire -->\
-            <li><a href="#">Voron Switchwire <i class="fa fa-caret-down"></i></a>\
-                <ul>\
-                    <li><a href="voron-color-configurator/voron-switchwire-default.html">Default</a></li>\
-                    <li><a href="voron-color-configurator/voron-switchwire-8accents.html">8 Accents</a></li>\
-                </ul>\
-            </li>\
-            <!-- Voron Legacy -->\
-            <li><a href="#">Voron Legacy <i class="fa fa-caret-down"></i></a>\
-                <ul>\
-                    <li><a href="voron-color-configurator/voron-legacy-default.html">Default</a></li>\
-                    <li><a href="voron-color-configurator/voron-legacy-8accents.html">8 Accents</a></li>\
-                </ul>\
-            </li>\
-            <!-- Download Center -->\
-            <li><a href="voron-color-configurator/voron-download-center.html">Download Center</a></li>\
-            <!-- About us -->\
             <li><a href="#">About us <i class="fa fa-caret-down"></i></a>\
                 <ul>\
-                    <li><a href="voron-color-configurator/contact.html">Contact</a></li>\
-                    <li><a href="voron-color-configurator/terms-of-use.html">Terms of use</a></li>\
+                    <li><a href="contact.html">Contact</a></li>\
+                    <li><a href="terms-of-use.html">Terms of use</a></li>\
+                    <li><a href="feedback.html">Feedback</a></li>\
                 </ul>\
             </li>\
-            <!-- Roadmap -->\
-            <li><a href="voron-color-configurator/roadmap.html">Roadmap</a></li>\
-            <!-- Support Me -->\
-            <li><a href="voron-color-configurator/support-me.html" style="font-weight: bold;">Support Me</a></li>\
+            <li><a href="roadmap.html">Roadmap</a></li>\
+            <li><a href="support-me.html" style="font-weight: bold;">Support Me</a></li>\
         </ul>\
     </nav>\
 </header>\
